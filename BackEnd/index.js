9ebb549f455c74ad4ab982c443de4b37831211e6
ujjawal sharma <112757910+Sharmaujjawal123@users.noreply.github.com>
2025-03-08 00:28:57 +0530

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,7 +1,24 @@
 require('dotenv').config()
-const app = require('./src/app')
+// const app = require('./src/app')
+
+const express = require('express');
+const aiRoutes = require('./src/routes/ai.routes')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
 
 
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.use('/ai', aiRoutes)
+
+module.exports = app
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000')
